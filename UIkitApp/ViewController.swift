ba4a859ff0dbb12184409f9f334f700e1754388b
Denis <majestycrew@gmail.com>
2023-03-07 21:12:43 +0300

    Initial Commit

---
+++ UIkitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkitApp
+//
+//  Created by Denis Shpagin on 07.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
